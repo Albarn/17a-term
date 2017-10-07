f68f6e0e17b085faad313d45ae77eb88a9c06932
Armin Albarn <elrib7j@gmail.com>
2017-10-07 13:16:02 +0300

    l2

--- Software designing/L1/Program.cs
+++ Software designing/L1/Program.cs
@@ -1,141 +1,204 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace L1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.Write(S(Console.ReadLine()));
+            //чтение выражения
+            string str = Console.ReadLine();
+
+            //вызов процедуры начального терминала на всей строке
+            Console.Write(S(str, 0, str.Length - 1));
+
+            //выход по нажатию кнопки
             Console.Read();
         }
 
-        static bool S(string s)
+        /// <summary>
+        /// распознавание списка векторов
+        /// </summary>
+        /// <param name="s">строка</param>
+        /// <param name="b">индекс первого элемента выражения</param>
+        /// <param name="e">индекс последнего элемента выражения</param>
+        /// <returns>результат анализа</returns>
+        static bool S(string s, int b, int e)
         {
-            if (s.Length == 0)
+
+            //если пустая цепочка возвращаем истину
+            if (b > e)
                 return true;
 
-            string l = "";
-            int i = 0;
-            for(;i<s.Length;i++)
-            {
-                l += s[i];
+            //иначе есть терминал А и В, ищем конец А
+            int i = b;
+            for (; i <= e && i < s.Length; i++)
+
+                //А - вектор, его конец - 
+                //закрывающаяся скобка
                 if (s[i] == ')') break;
-            }
-            if (!A(l)) return false;
 
-            l = "";
-            i++;
-            for (; i < s.Length; i++)
-                l += s[i];
+            //если это не вектор возвращаем ложь
+            if (!A(s, b, i)) return false;
 
-            return B(l);
+            //иначе возвращаем р-т проверки остальной части
+            return B(s, i + 1, s.Length - 1);
         }
 
-        static bool B(string s)
+        /// <summary>
+        /// распознавание следующего вектора
+        /// </summary>
+        /// <param name="s">строка</param>
+        /// <param name="b">индекс первого элемента выражения</param>
+        /// <param name="e">индекс последнего элемента выражения</param>
+        /// <returns>результат анализа</returns>
+        static bool B(string s, int b, int e)
         {
-            if (s.Length == 0)
+            //если предыдущий вектор был последним
+            //возвращаем истину
+            if (b > e)
                 return true;
-            if (s[0] != ',')
-                return false;
 
-            string l = "";
-            int i = 1;
-            for(;i<s.Length;i++)
+            //первый знак должен быть запятой
+            if (b > s.Length - 1 || s[b] != ',')
             {
-                l += s[i];
+                Console.WriteLine("На позиции " + (b + 1) + " ожидался символ ','");
+                return false;
+            }
+
+            //ищем конец вектора
+            int i = b + 1;
+            for (; i <= e && i < s.Length; i++)
                 if (s[i] == ')') break;
-            }
-            if (!A(l)) return false;
 
-            l = "";
-            i++;
-            for (; i < s.Length; i++)
-                l += s[i];
+            //если это не вектор возвращаем ложь
+            if (!A(s, b + 1, i)) return false;
 
-            return B(l);
+            //иначе возвращаем р-т проверки остальной части
+            return B(s, i + 1, s.Length - 1);
         }
 
-        static bool A(string s)
+        /// <summary>
+        /// распознавание вектора
+        /// </summary>
+        /// <param name="s">строка</param>
+        /// <param name="b">индекс первого элемента выражения</param>
+        /// <param name="e">индекс последнего элемента выражения</param>
+        /// <returns>результат анализа</returns>
+        static bool A(string s, int b, int e)
         {
-            if (s.Length == 0)
-                return false;
-            if (s[0] != '(')
-                return false;
-
-            string l = "";
-            int i = 1;
-            for (; i < s.Length && s[i] != ','; i++)
-                l += s[i];
-            if (!Z(l)) return false;
-
-            l = "";
-            i++;
-            for (; i < s.Length && s[i] != ','; i++)
-                l += s[i];
-            if (!Z(l)) return false;
-
-            l = "";
-            i++;
-            for (; i < s.Length && s[i] != ')'; i++)
-                l += s[i];
-            if (!Z(l)) return false;
-
-            if (i == s.Length - 1 && s[i] == ')')
-                return true;
-            else
-                return false;
-        }
-
-        static bool Z(string s)
-        {
-            if (s.Length == 0)
-                return false;
-            if (s == "0")
-                return true;
-            if (s[0] == '-')
+            //вектор не может быть пустой цепочкой,
+            //первый символ должен быть открывающей скобкой
+            if (b > e || b > s.Length - 1 || s[b] != '(')
             {
-                string l = "";
-                for (int i = 1; i < s.Length; i++)
-                    l += s[i];
-                return N(l);
-            }
-            else
-                return N(s);
-        }
-
-        static bool N(string s)
-        {
-            if (s.Length == 0)
+                Console.WriteLine("На позиции " + (b + 1) + " ожидался символ '('");
                 return false;
-            else if (s[0] >= '1' && s[0] <= '9')
-            {
-                string l = "";
-                for (int i = 1; i < s.Length; i++)
-                    l += s[i];
-                return D(l);
             }
-            else
-                return false;
-        }
 
-        static bool D(string s)
-        {
-            if (s.Length == 0)
+            //от скобки до запятой первое целое число
+            int i = b + 1;
+            for (; i < s.Length && i <= e && s[i] != ','; i++) ;
+            if (!Z(s, b + 1, i - 1)) return false;
+
+            //от запятой до запятой второе число
+            i++;
+            b = i;
+            for (; i < s.Length && i <= e && s[i] != ','; i++) ;
+            if (!Z(s, b, i - 1)) return false;
+
+            //от запятой до скобки последнее число
+            i++;
+            b = i;
+            for (; i < s.Length && i <= e && s[i] != ')'; i++) ;
+            if (!Z(s, b, i - 1)) return false;
+
+            //если конец вектора правилен возвращаем истину
+            if (i < s.Length && i == e && s[i] == ')')
                 return true;
-            else if (s[0] >= '0' && s[0] <= '9')
-            {
-                string l = "";
-                for (int i = 1; i < s.Length; i++)
-                    l += s[i];
-                return D(l);
-            }
+            //иначе сообщаем об ошибке в выражении
             else
+            {
+                Console.WriteLine("На позиции " + (i + 1) + " ожидался символ ')'");
                 return false;
+            }
+        }
+
+        /// <summary>
+        /// распознавание целого числа
+        /// </summary>
+        /// <param name="s">строка</param>
+        /// <param name="b">индекс первого элемента выражения</param>
+        /// <param name="e">индекс последнего элемента выражения</param>
+        /// <returns>результат анализа</returns>
+        static bool Z(string s, int b, int e)
+        {
+            //целое число не может быть пустой цепочкой
+            if (b > e || b > s.Length - 1)
+            {
+                Console.WriteLine("На позиции " + (b + 1) + " ожидалась цифра от 0 до 9 или символ '-'");
+                return false;
+            }
+
+            //если выражение состоит только из нуля
+            //возвращаем истину
+            if (s[b] == '0' && b == e)
+                return true;
+            //если первый знак минус
+            //не включаем его в проверку N
+            if (s[b] == '-')
+                return N(s, b + 1, e);
+            //иначе проверяем все выражение на N
+            else
+                return N(s, b, e);
+        }
+
+        /// <summary>
+        /// распознавание натурального числа
+        /// </summary>
+        /// <param name="s">строка</param>
+        /// <param name="b">индекс первого элемента выражения</param>
+        /// <param name="e">индекс последнего элемента выражения</param>
+        /// <returns>результат анализа</returns>
+        static bool N(string s, int b, int e)
+        {
+            //натуральное число не может быть пустой цепочкой
+            if (b > e)
+            {
+                Console.WriteLine("На позиции " + (b + 1) + " ожидалась цифра от 1 до 9");
+                return false;
+            }
+            //первый знак не должен быть нулем
+            else if (s[b] >= '1' && s[b] <= '9')
+                return D(s, b + 1, e);
+            else
+            {
+                Console.WriteLine("На позиции " + (b + 1) + " ожидалась цифра от 1 до 9");
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// распознавание следующей цифры
+        /// </summary>
+        /// <param name="s">строка</param>
+        /// <param name="b">индекс первого элемента выражения</param>
+        /// <param name="e">индекс последнего элемента выражения</param>
+        /// <returns>результат анализа</returns>
+        static bool D(string s, int b, int e)
+        {
+            //если предыдущая цифра была последней
+            //возвращаем истину
+            if (b > e)
+                return true;
+
+            //следующий знак должен быть цифрой
+            else if (s[b] >= '0' && s[b] <= '9')
+                return D(s, b + 1, e);
+            else
+            {
+                Console.WriteLine("На позиции " + (b + 1) + " ожидалась цифра от 0 до 9");
+                return false;
+            }
         }
     }
 }
---
+++ Software designing/L2/Program.cs
@@ -0,0 +1,236 @@
+﻿using System;
+
+namespace L2
+{
+    class Program
+    {
+        static void Main()
+        {
+            //чтение выражения
+            string str = Console.ReadLine();
+
+            //вызов процедуры начального терминала на всей строке
+            int b = 0;
+            Console.Write(S(str, 0));
+
+            //выход по нажатию кнопки
+            Console.Read();
+        }
+
+        /// <summary>
+        /// распознавание списка векторов
+        /// </summary>
+        /// <param name="s">строка</param>
+        /// <param name="b">индекс первого элемента выражения</param>
+        /// <returns>результат анализа</returns>
+        static bool S(string s, int b)
+        {
+            s22:
+            if (s.Length == 0)
+                return true;
+            s23:
+            if (b < s.Length && s[b] == '(')
+            {
+                if (!A(s, ref b))
+                    return false;
+                else b++;
+            }
+            else
+            {
+                Console.WriteLine("На позиции " + (b + 1) + " ожидался символ '('");
+                return false;
+            }
+            s24:
+            if (!B(s, ref b))
+                return false;
+            s25:
+            if (b < s.Length)
+            {
+                Console.WriteLine("На позиции " + (b + 1) + " ожидался символ ','");
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// распознавание следующего вектора
+        /// </summary>
+        /// <param name="s">строка</param>
+        /// <param name="b">индекс первого элемента выражения</param>
+        /// <returns>результат анализа</returns>
+        static bool B(string s, ref int b)
+        {
+            s18:
+            if (b < s.Length && s[b] == ',')
+            {
+                b++;
+                goto s20;
+            }
+            s19:
+            return true;
+            s20:
+            if (b < s.Length && s[b] == '(')
+            {
+                if (!A(s, ref b))
+                    return false;
+                else b++;
+            }
+            else
+            {
+                Console.WriteLine("На позиции " + (b + 1) + " ожидался символ '('");
+                return false;
+            }
+            s21:
+            return B(s, ref b);
+        }
+
+        /// <summary>
+        /// распознавание вектора
+        /// </summary>
+        /// <param name="s">строка</param>
+        /// <param name="b">индекс первого элемента выражения</param>
+        /// <returns>результат анализа</returns>
+        static bool A(string s, ref int b)
+        {
+            s10:
+            if (s[b] == '(')
+                b++;
+            else
+            {
+                Console.WriteLine("На позиции " + (b + 1) + " ожидался символ '('");
+                return false;
+            }
+            s11:
+            if (b < s.Length && s[b] == '-' || (s[b] >= '0' && s[b] <= '9'))
+            {
+                if (!Z(s, ref b))
+                    return false;
+            }
+            else
+            {
+                Console.WriteLine("На позиции " + (b + 1) +
+                    " ожидалась цифра или символ '-'");
+                return false;
+            }
+            s12:
+            if (b < s.Length && s[b] == ',')
+                b++;
+            else
+            {
+                Console.WriteLine("На позиции " + (b + 1) + " ожидался символ ','");
+                return false;
+            }
+            s13:
+            if (b < s.Length && (s[b] == '-' || (s[b] >= '0' && s[b] <= '9')))
+            {
+                if (!Z(s, ref b))
+                    return false;
+            }
+            else
+            {
+                Console.WriteLine("На позиции " + (b + 1) +
+                    " ожидалась цифра или символ '-'");
+                return false;
+            }
+            s14:
+            if (b < s.Length && s[b] == ',')
+                b++;
+            else
+            {
+                Console.WriteLine("На позиции " + (b + 1) + " ожидался символ ','");
+                return false;
+            }
+            s15:
+            if (b < s.Length && (s[b] == '-' || (s[b] >= '0' && s[b] <= '9')))
+            {
+                if (!Z(s, ref b))
+                    return false;
+            }
+            else
+            {
+                Console.WriteLine("На позиции " + (b + 1) +
+                    " ожидалась цифра или символ '-'");
+                return false;
+            }
+            s16:
+            if (!(b < s.Length) || s[b] != ')')
+            {
+                Console.WriteLine("На позиции " + (b + 1) + " ожидался символ ')'");
+                return false;
+            }
+            s17:
+            return true;
+        }
+
+        /// <summary>
+        /// распознавание целого числа
+        /// </summary>
+        /// <param name="s">строка</param>
+        /// <param name="b">индекс первого элемента выражения</param>
+        /// <returns>результат анализа</returns>
+        static bool Z(string s, ref int b)
+        {
+            s6:
+            if (s[b] == '0')
+            {
+                b++;
+                goto s9;
+            }
+            s7:
+            if (b < s.Length && s[b] == '-')
+                b++;
+            s8:
+            if (b < s.Length && s[b] >= '1' && s[b] <= '9')
+                return N(s, ref b);
+            else
+            {
+                Console.WriteLine("На позиции " + (b + 1) + " ожидалась цифра от 1 до 9");
+                return false;
+            }
+            s9:
+            return true;
+        }
+
+        /// <summary>
+        /// распознавание натурального числа
+        /// </summary>
+        /// <param name="s">строка</param>
+        /// <param name="b">индекс первого элемента выражения</param>
+        /// <returns>результат анализа</returns>
+        static bool N(string s, ref int b)
+        {
+            s3:
+            if (s[b] >= '1' && s[b] <= '9')
+                b++;
+            else
+            {
+                Console.WriteLine("На позиции " + (b + 1) + " ожидалась цифра от 1 до 9");
+                return false;
+            }
+            s4:
+            if (b < s.Length && s[b] >= '0' && s[b] <= '9')
+                return D(s, ref b);
+
+            s5:
+            return true;
+        }
+
+        /// <summary>
+        /// распознавание следующей цифры
+        /// </summary>
+        /// <param name="s">строка</param>
+        /// <param name="b">индекс первого элемента выражения</param>
+        /// <returns>результат анализа</returns>
+        static bool D(string s, ref int b)
+        {
+            s1:
+            if (b < s.Length && s[b] >= '0' && s[b] <= '9')
+            {
+                b++;
+                goto s1;
+            }
+            s2:
+            return true;
+        }
+    }
+}
