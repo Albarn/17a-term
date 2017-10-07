578f0346db163be9fbecb8d43a69c09b63441497
Armin Albarn <elrib7j@gmail.com>
2017-10-07 11:58:53 +0300

    init

---
+++ ASD_Course_Project/Class Library/Matrix.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Class_Library
+{
+    public class Matrix:IEnumerable<Cell>
+    {
+        List<Cell> cells;
+
+        public int Count { get
+            {
+                return cells.Count;
+            } }
+
+        public void Add(Cell c)
+        {
+            cells.Add(c);
+        }
+
+        public void Remove(Cell c)
+        {
+            cells.Remove(c);
+        }
+
+        public Matrix()
+        {
+            cells = new List<Cell>();
+        }
+
+        public IEnumerator<Cell> GetEnumerator()
+        {
+            return ((IEnumerable<Cell>)cells).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<Cell>)cells).GetEnumerator();
+        }
+
+        public Cell this[int row, int col]
+        {
+            get {
+                foreach (Cell cell in cells)
+                    if (cell.Row == row && cell.Col == col)
+                        return cell;
+                return null;
+            }
+        }
+    }
+}
