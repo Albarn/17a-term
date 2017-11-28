9b1ec32cfb74f1b2e2cfff6fd7fcb4e2d93e2df4
Armin Albarn <elrib7j@gmail.com>
2017-11-29 00:38:12 +0200

    comments

--- ASD_Course_Project/WinForm App/Form1.Designer.cs
+++ ASD_Course_Project/WinForm App/Form1.Designer.cs
@@ -65,7 +65,6 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(398, 323);
             this.tableLayoutPanel1.TabIndex = 7;
-            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
             // 
             // dataGridView1
             // 
--- ASD_Course_Project/WinForm App/Form1.cs
+++ ASD_Course_Project/WinForm App/Form1.cs
@@ -6,28 +6,36 @@ namespace WinForm_App
 {
     public partial class Form1 : Form
     {
+        //левая и правая часть системы
         Matrix B = new Matrix();
         Matrix A = new Matrix();
+
+        //генерируется ли таблица в данный момент
         bool gen = false;
 
         public Form1()
         {
             InitializeComponent();
+
+            //создаем матрицу 1х1
             sizeNumericUpDown_ValueChanged(null, null);
         }
 
-        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
+        //перестраиваем таблицу, по изменению значения в sizeNumericUpDown
         private void sizeNumericUpDown_ValueChanged(object sender, EventArgs e)
         {
+            //извлекаем размер
             int size = (int)sizeNumericUpDown.Value;
+
+            //чистим таблицы
             dataGridView1.Columns.Clear();
             dataGridView2.Columns.Clear();
+
+            //заголовок таблицы
             dataGridView2.Columns.Add("", "X");
             dataGridView2.Columns[dataGridView2.Columns.Count - 1].Width = 50;
+
+            //делаем столько колонок и столбцов, сколько указал пользователь
             for (int i = 0; i < size; i++)
             {
                 dataGridView1.Columns.Add("c" + i, (i + 1).ToString());
@@ -37,17 +45,24 @@ namespace WinForm_App
                 dataGridView2.Rows.Add("", "");
                 dataGridView2.Rows[dataGridView2.Rows.Count - 1].HeaderCell.Value = (i + 1).ToString();
             }
+
+            //заголовок для правой части выражения
             dataGridView1.Columns.Add("a", "A");
             dataGridView1.Columns[dataGridView1.Columns.Count - 1].Width = 50;
+
+            //объявление матриц
             B = new Matrix();
             A = new Matrix();
         }
 
+        //запись изменений из таблицы в матрицу
         private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
         {
+            //если нажата активная ячейка и таблица не генерируется в данный момент
             if (!gen && e.ColumnIndex >= 0 && e.RowIndex >= 0 && dataGridView1[e.ColumnIndex, e.RowIndex] != null)
                 if (e.ColumnIndex != dataGridView1.Columns.Count - 1)
                 {
+                    //если выбрана не последняя колонка, записываем в В
                     float f;
                     float.TryParse(
                         dataGridView1[e.ColumnIndex, e.RowIndex].Value.ToString(), out f);
@@ -55,7 +70,7 @@ namespace WinForm_App
                 }
                 else
                 {
-
+                    //иначе в А
                     float f;
                     float.TryParse(
                         dataGridView1[e.ColumnIndex, e.RowIndex].Value.ToString(), out f);
@@ -63,14 +78,21 @@ namespace WinForm_App
                 }
         }
 
+        //генерация случайной системы
         private void randButton_Click(object sender, EventArgs e)
         {
+            //отмечаем что матрица сейчас генерируется
             gen = true;
+
+            //создаем матрицу и записываем ее в таблицу
             int size = (int)sizeNumericUpDown.Value;
             B = new Matrix(size, (float)trackBar1.Value / 20);
             for (int i = 0; i < size; i++)
                 for (int j = 0; j < size; j++)
                     dataGridView1[j, i].Value = B[i + 1, j + 1].ToString("F2");
+
+            //тоже самое для правой части,
+            //только генерируем вручную
             A = new Matrix();
             int count = (int)(size * (float)trackBar1.Value / 20);
             Random f = new Random(DateTime.Now.Millisecond);
@@ -85,8 +107,10 @@ namespace WinForm_App
             gen = false;
         }
 
+        //решаем систему
         private void resolveButton_Click(object sender, EventArgs e)
         {
+            //метод обращения изменяет матрицу, поэтому копируем исходную
             Matrix V = B.Copy();
             if (!V.Invert() || B.Size != (int)sizeNumericUpDown.Value)
             {
@@ -96,7 +120,12 @@ namespace WinForm_App
                     "заполните главную диагональ таблицы");
                 return;
             }
+
+            //получаем решение перемножением обращенной матрицы
+            //на правую часть
             Matrix X = Matrix.MultiplyMatrixes(V, A);
+
+            //запись решения в вектор справа
             for (int i = 0; i < B.Size; i++)
                 dataGridView2.Rows[i].Cells[0].Value =
                     X[i + 1, 1].ToString("F2");
